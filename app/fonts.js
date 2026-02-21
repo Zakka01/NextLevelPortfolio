5877d686569da41b6c5d78eeec6e826c24b09601
Ahrabare Zakariae <133920536+Zakka01@users.noreply.github.com>
2026-02-21 02:57:10 +0000

    add fonts

--- app/fonts.js
+++ app/fonts.js
@@ -2,88 +2,88 @@ import localFont from "next/font/local";
 
 export const polySansBulky = localFont({
   src: [
-    { path: "./font/polysanstrial-bulky.otf", weight: "400", style: "normal" },
-    { path: "./font/polysansitalictrial-bulkyitalic.otf", weight: "400", style: "italic" },
+    { path: "public/font/polysanstrial-bulky.otf", weight: "400", style: "normal" },
+    { path: "public/font/polysansitalictrial-bulkyitalic.otf", weight: "400", style: "italic" },
   ],
   variable: "--font-polysans-bulky",
 });
 
 export const polySansBulkyMono = localFont({
   src: [
-    { path: "./font/polysanstrial-bulkymono.otf", weight: "400", style: "normal" },
-    { path: "./font/polysansitalictrial-bulkymonoitalic.otf", weight: "400", style: "italic" },
+    { path: "public/font/polysanstrial-bulkymono.otf", weight: "400", style: "normal" },
+    { path: "public/font/polysansitalictrial-bulkymonoitalic.otf", weight: "400", style: "italic" },
   ],
   variable: "--font-polysans-bulkymono",
 });
 
 export const polySansBulkyWide = localFont({
   src: [
-    { path: "./font/polysanstrial-bulkywide.otf", weight: "400", style: "normal" },
-    { path: "./font/polysansitalictrial-bulkywideitalic.otf", weight: "400", style: "italic" },
+    { path: "public/font/polysanstrial-bulkywide.otf", weight: "400", style: "normal" },
+    { path: "public/font/polysansitalictrial-bulkywideitalic.otf", weight: "400", style: "italic" },
   ],
   variable: "--font-polysans-bulkywide",
 });
 
 export const polySansMedianMono = localFont({
   src: [
-    { path: "./font/polysanstrial-medianmono.otf", weight: "400", style: "normal" },
-    { path: "./font/polysansitalictrial-medianmonoitalic.otf", weight: "400", style: "italic" },
+    { path: "public/font/polysanstrial-medianmono.otf", weight: "400", style: "normal" },
+    { path: "public/font/polysansitalictrial-medianmonoitalic.otf", weight: "400", style: "italic" },
   ],
   variable: "--font-polysans-medianmono",
 });
 
 export const polySansMedianWide = localFont({
   src: [
-    { path: "./font/polysanstrial-medianwide.otf", weight: "400", style: "normal" },
-    { path: "./font/polysansitalictrial-medianwideitalic.otf", weight: "400", style: "italic" },
+    { path: "public/font/polysanstrial-medianwide.otf", weight: "400", style: "normal" },
+    { path: "public/font/polysansitalictrial-medianwideitalic.otf", weight: "400", style: "italic" },
   ],
   variable: "--font-polysans-medianwide",
 });
 
 export const polySansNeutral = localFont({
   src: [
-    { path: "./font/polysanstrial-neutral.otf", weight: "400", style: "normal" },
-    { path: "./font/polysansitalictrial-neutralitalic.otf", weight: "400", style: "italic" },
+    { path: "public/font/polysanstrial-neutral.otf", weight: "400", style: "normal" },
+    { path: "public/font/polysansitalictrial-neutralitalic.otf", weight: "400", style: "italic" },
   ],
   variable: "--font-polysans-neutral",
 });
 
 export const polySansNeutralMono = localFont({
   src: [
-    { path: "./font/polysanstrial-neutralmono.otf", weight: "400", style: "normal" },
-    { path: "./font/polysansitalictrial-neutralmonoitalic.otf", weight: "400", style: "italic" },
+    { path: "public/font/polysanstrial-neutralmono.otf", weight: "400", style: "normal" },
+    { path: "public/font/polysansitalictrial-neutralmonoitalic.otf", weight: "400", style: "italic" },
   ],
   variable: "--font-polysans-neutralmono",
 });
 
 export const polySansNeutralWide = localFont({
   src: [
-    { path: "./font/polysanstrial-neutralwide.otf", weight: "400", style: "normal" },
-    { path: "./font/polysansitalictrial-neutralwideitalic.otf", weight: "400", style: "italic" },
+    { path: "public/font/polysanstrial-neutralwide.otf", weight: "400", style: "normal" },
+    { path: "public/font/polysansitalictrial-neutralwideitalic.otf", weight: "400", style: "italic" },
   ],
   variable: "--font-polysans-neutralwide",
 });
 
 export const polySansSlim = localFont({
   src: [
-    { path: "./font/polysanstrial-slim.otf", weight: "400", style: "normal" },
-    { path: "./font/polysansitalictrial-slimitalic.otf", weight: "400", style: "italic" },
+    { path: "public/font/polysanstrial-slim.otf", weight: "400", style: "normal" },
+    { path: "public/font/polysansitalictrial-slimitalic.otf", weight: "400", style: "italic" },
   ],
   variable: "--font-polysans-slim",
 });
 
 export const polySansSlimMono = localFont({
   src: [
-    { path: "./font/polysanstrial-slimmono.otf", weight: "400", style: "normal" },
-    { path: "./font/polysansitalictrial-slimmonoitalic.otf", weight: "400", style: "italic" },
+    { path: "public/font/polysanstrial-slimmono.otf", weight: "400", style: "normal" },
+    { path: "public/font/polysansitalictrial-slimmonoitalic.otf", weight: "400", style: "italic" },
   ],
   variable: "--font-polysans-slimmono",
 });
 
 export const polySansSlimWide = localFont({
   src: [
-    { path: "./font/polysanstrial-slimwide.otf", weight: "400", style: "normal" },
-    { path: "./font/polysansitalictrial-slimwideitalic.otf", weight: "400", style: "italic" },
+    { path: "public/font/polysanstrial-slimwide.otf", weight: "400", style: "normal" },
+    { path: "public/font/polysansitalictrial-slimwideitalic.otf", weight: "400", style: "italic" },
   ],
   variable: "--font-polysans-slimwide",
 });
